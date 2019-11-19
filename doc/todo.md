6effd6223763e3fb6576a61d0ddca359fb50d85b
Bernhard Lukassen <bl@bernhard-lukassen.com>
2019-11-19 21:58:21 +0100

    evolux.dyncomponent fixes and new classes for reliant nodes

--- doc/todo.md
+++ doc/todo.md
@@ -4,3 +4,7 @@ ToDo
 - rename package
     - name it more like 'Event'
   
+- enable subscription to historic events
+    - e.g. when a component listens to the installation of a component which was installed already
+    
+- conditions for listeners
--- index.mjs
+++ index.mjs
@@ -7,11 +7,11 @@ import {Matter} from "../evolux.matter/index.mjs";
  */
 
 
-const myuniverse = (() => {
+const myuniverse = () => {
     // this is a cryptic way to get the 'global' object or 'window' in strict mode. direct code references will throw an error
     const space = (1,eval)("this");
     return space.universe ? space.universe : space;
-})();
+};
 
 import Registry                     from "./lib/registry.mjs";
 export { default as Registry }      from './lib/registry.mjs';
@@ -20,12 +20,12 @@ export { default as EventEmitter }  from './lib/eventemitter.mjs'
 export const service = {
     install() {
         console.log('** pubsub install()');
-        myuniverse.pubsub = new Registry();
+        myuniverse().pubsub = new Registry();
     },
 
     uninstall() {
         console.log('** pubsub uninstall()');
-        myuniverse.pubsub.unregisterAll();
+        myuniverse().pubsub.unregisterAll();
         delete handle().pubsub;
     },
 
@@ -36,20 +36,20 @@ export const service = {
 
     start() {
         console.log('** pubsub start()');
-        myuniverse.pubsub.resumeAll();
+        myuniverse().pubsub.resumeAll();
     },
 
     stop() {
         console.log('** pubsub stop()');
-        myuniverse.pubsub.pauseAll();
+        myuniverse().pubsub.pauseAll();
     },
 
     update() {
         console.log('** pubsub update()');
         this.stop();
-        let saved = myuniverse.pubsub.entries;
+        let saved = myuniverse().pubsub.entries;
         this.uninstall();
         this.install();
-        myuniverse.pubsub.reregister(...saved);
+        myuniverse().pubsub.reregister(...saved);
     }
 };
