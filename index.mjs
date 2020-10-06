029b7c8b97bbf292b745cc204b95d7302ade4ea2
Bernhard Lukassen <bl@bernhard-lukassen.com>
2020-10-06 20:48:24 +0200

    removed myuniverse() method, use directly universe

--- index.mjs
+++ index.mjs
@@ -9,37 +9,37 @@ export { default as EventEmitter }  from './lib/eventemitter.mjs'
 import Registry                     from "./lib/registry.mjs";
 export { default as Registry }      from './lib/registry.mjs';
 
-import { myuniverse, tservices }     from "/evolux.universe";
+import { tservices }                from "/evolux.universe";
 
 export const service = {
     install() {
-        myuniverse().logger.debug('** pubsub install()');
+        universe.logger.debug('** pubsub install()');
         if (!tservices().pubsub) tservices().pubsub = new Registry();
     },
 
     uninstall() {
-        myuniverse().logger.debug('** pubsub uninstall()');
-        myuniverse().pubsub.unregisterAll();
+        universe.logger.debug('** pubsub uninstall()');
+        universe.pubsub.unregisterAll();
         delete tservices().pubsub;
     },
 
     resolve() {
-        myuniverse().logger.debug('** pubsub resolve()');
+        universe.logger.debug('** pubsub resolve()');
         // nothing to do
     },
 
     start() {
-        myuniverse().logger.debug('** pubsub start()');
+        universe.logger.debug('** pubsub start()');
         tservices().pubsub.resumeAll();
     },
 
     stop() {
-        myuniverse().logger.debug('** pubsub stop()');
+        universe.logger.debug('** pubsub stop()');
         tservices().pubsub.pauseAll();
     },
 
     update() {
-        myuniverse().logger.debug('** pubsub update()');
+        universe.logger.debug('** pubsub update()');
 /*
         this.stop();
         let saved = tservices().pubsub.entries;
